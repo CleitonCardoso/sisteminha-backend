4530eb85b20f93a0abed71bd5ae2b8f20411d2e7
CleitonCardoso <cleitoncardoso_joy@hotmail.com>
2018-01-22 22:01:12 -0200

    backend repo movido

---
+++ src/main/java/com/sisteminha/entities/User.java
@@ -0,0 +1,30 @@
+package com.sisteminha.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false, unique = true)
+	private String username;
+	@Column(nullable = false)
+	private String password;
+	private boolean active;
+
+}
